918f8f5b2365f1477c208528b8114a694ca4645c
PrathmeshS <prs.lowell@gmail.com>
2023-09-10 14:46:21 -0400

    Worked on Assign3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,2 +1,14 @@
-(* #use "./../assign0.ml";; *)
+(* Must support all int, including 0 and negative *)
+
+let rec build (number:int) (word: string):string = if number <> 0 then build (number/10) ((string_of_int (number mod 10)) ^ word) else word;;
+
+let int2str(i0: int): string = 
+if i0 = 0 then "0" else 
+if (i0 <0) then 
+ (build) i0 "" 
+
+else 
+  (build) i0 "" 
+
+;;
 
