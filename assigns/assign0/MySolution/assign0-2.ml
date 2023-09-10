970b3493f7dc4aa9a0429715da7fc2ac511fa2ac
PrathmeshS <prs.lowell@gmail.com>
2023-09-09 22:58:10 -0400

    Fixed syntax of P2

--- assigns/assign0/MySolution/assign0-2.ml
+++ assigns/assign0/MySolution/assign0-2.ml
@@ -1,8 +1,5 @@
+let rec divisible (old:int)(newer:int) = if newer <= 1 then true else if old mod newer = 0 then false else divisible old (newer-1);;
 
+let isPrime(n0:int): bool = if (n0>=2) then (divisible) (n0) (n0-1) else false;;
 
-let rec divisible (old:int)(newer:int) = if newer = 1 then true else if old mod newer = 0 then false else divisible old (newer-1);;
-
-let isPrime(n0:int):bool = if (n0>=2) then (divisible) (n0) (n0-1) else false;;
-
-let num = 7;;
-let answer = (divisible) (num) (num-1);;
+let num = (isPrime) 7 ;;
