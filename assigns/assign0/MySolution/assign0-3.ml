309fa29d2ce84956b84e107b5a149f46a68427ba
PrathmeshS <prs.lowell@gmail.com>
2023-09-10 17:00:17 -0400

    Finished P3, decided to use init and worked to create the concat function

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,14 +1,30 @@
 (* Must support all int, including 0 and negative *)
+#use "../assign0.ml";;
 
-let rec build (number:int) (word: string):string = if number <> 0 then build (number/10) ((string_of_int (number mod 10)) ^ word) else word;;
+let rec build (number:int) (word: string):string = if number <> 0 then 
+  if number > 0 then  build (number/10) (concat (str((chr((number mod 10)+48)))) (word))
+  else build (number/10) (concat (str((chr(((number mod 10) * -1)+48)))) (word))
+   else word;;
 
 let int2str(i0: int): string = 
 if i0 = 0 then "0" else 
-if (i0 <0) then 
- (build) i0 "" 
-
-else 
-  (build) i0 "" 
-
+    if i0 >0 then
+    (build) i0 ""
+    else 
+      concat ("-")  ((build) i0 "")
 ;;
 
+let concat str1 str2 =
+  let len1 = string_length str1 in
+  let len2 = string_length str2 in
+  let result_len = len1 + len2 in
+
+  let result = string_init result_len (fun i ->
+    if i < len1 then
+      str1.[i]
+    else
+      str2.[i - len1]
+  ) in
+
+  result
+;;
