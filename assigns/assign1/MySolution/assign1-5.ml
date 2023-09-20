b31da5f67f20a2ba895fdd43145c39ee35a820dd
PrathmeshS <prs.lowell@gmail.com>
2023-09-20 18:51:35 -0400

    Finished P1-6

--- assigns/assign1/MySolution/assign1-5.ml
+++ assigns/assign1/MySolution/assign1-5.ml
@@ -1,1 +1,35 @@
+(* ****** ****** *)
+(* ****** ****** *)
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
+
+let rec largest(whole:string)(sub:string)(index:int):string = 
+if (index >= string_length(whole))
+  then sub
+else 
+  if (string_length(sub) =0) 
+    then 
+      let including =  largest(whole)(string_snoc(sub)(string_get_at(whole)(index)))(index+1) in
+          let excluding = largest(whole)(sub)(index+1) in
+          if (string_length (including) > string_length (excluding)) then
+          including
+          else 
+          excluding
+  else 
+    if (string_get_at(whole)(index) >= string_get_at(sub)(string_length(sub)-1)) 
+      then
+        let including =  largest(whole)(string_snoc(sub)(string_get_at(whole)(index)))(index+1) in
+          let excluding = largest(whole)(sub)(index+1) in
+          if (string_length (including) > string_length (excluding)) then
+          including
+          else 
+          excluding
+      else
+        ""
+;;
+
+let string_longest_ascend(xs: string): string = 
+  if (string_length (xs) <2)
+    then xs 
+  else 
+    largest(xs)("")(0)
+;;
