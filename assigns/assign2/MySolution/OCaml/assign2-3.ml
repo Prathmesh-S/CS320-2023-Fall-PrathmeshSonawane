91199966b3740c46e6fb79a78a58b9a690c49c5a
PrathmeshS <prs.lowell@gmail.com>
2023-09-27 10:04:44 -0400

    Fixed 2.3

--- assigns/assign2/MySolution/OCaml/assign2-3.ml
+++ assigns/assign2/MySolution/OCaml/assign2-3.ml
@@ -5,5 +5,5 @@
 
 let
 foldleft_to_iforeach (foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach =
-  fun (xs)(work) -> let_ = foldleft xs 0 (fun(idx)(x0) ->((work (idx)(x0)); idx+1)) in ()
+  fun (xs)(work) -> let _ = foldleft xs 0 (fun(idx)(x0) ->((work (idx)(x0)); idx+1)) in ()
 ;;
