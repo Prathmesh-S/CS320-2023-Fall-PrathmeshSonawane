cf916a46eea772def8cc6721b72e063905a82720
PrathmeshS <prs.lowell@gmail.com>
2023-09-27 09:52:45 -0400

    First draft of 2-6

--- assigns/assign2/MySolution/Python/assign2_6.py
+++ assigns/assign2/MySolution/Python/assign2_6.py
@@ -1,8 +1,8 @@
 #Problem 6
 
 def string_merge(x,y):
-    len1 = len(x)
-    len2 = len(y)
+    len1 = string_len(x)
+    len2 = string_len(y)
     
     return(merge(x, y, len1, len2))
 
@@ -15,8 +15,15 @@ def merge(x,y,len1,len2):
     elif (len2<1):
         return (x)
     elif (x[0] <y[0]):
-        return x[0] + merge(x[1:len(x)],y,len1-1,len2)
+        return x[0] + merge(string_get_at(x,1,string_len(x)),y,len1-1,len2)
     else:
-        return y[0] + merge(x,y[1:len(y)],len1,len2-1)    
+        return y[0] + merge(x,string_get_at(y,1,string_len(y)),len1,len2-1)    
     
-    
+
+def string_len(x):
+    return len(x)
+
+def string_get_at(x, start, finish):
+    return x[start:finish]
+
+print(string_merge("135", "2468"))
