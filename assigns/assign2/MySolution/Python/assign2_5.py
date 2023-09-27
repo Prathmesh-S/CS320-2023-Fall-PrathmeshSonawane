da106b0e9668eb3f6d34ee698f00ed9c026dfcd5
PrathmeshS <prs.lowell@gmail.com>
2023-09-27 18:17:38 -0400

    First try of 2-5

---
+++ assigns/assign2/MySolution/Python/assign2_5.py
@@ -0,0 +1,17 @@
+####################################################
+import sys
+sys.path.append("./../../../../classlib/Python")
+from MyPython import *
+
+#Problem 5
+
+def fnlist_make_fwork(func):
+    newList = fnlist_nil()
+
+    def work(node):
+        newNode = fnlist_cons(x0, newList)
+    work(func)
+
+    return(newList)
+
+print(fnlist_make_fwork(lambda work: work(0)).cons) 
