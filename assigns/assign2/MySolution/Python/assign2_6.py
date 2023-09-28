b3eeefc56ca48d9c4a0671e67c17c97260ee54e1
PrathmeshS <prs.lowell@gmail.com>
2023-09-27 21:07:34 -0400

    Translated code more closely

--- assigns/assign2/MySolution/Python/assign2_6.py
+++ assigns/assign2/MySolution/Python/assign2_6.py
@@ -4,20 +4,23 @@ def string_merge(x,y):
     len1 = string_len(x)
     len2 = string_len(y)
     
-    return(merge(x, y, len1, len2))
+    return(string_make_fwork(x, y, len1, len2))
 
 
 
-def merge(x,y,len1,len2):
+def string_make_fwork(x,y,len1,len2):
     
-    if (len1 <1):
-        return(y)
-    elif (len2<1):
-        return (x)
-    elif (x[0] <y[0]):
-        return x[0] + merge(string_get_at(x,1,string_len(x)),y,len1-1,len2)
-    else:
-        return y[0] + merge(x,string_get_at(y,1,string_len(y)),len1,len2-1)    
+    def foreach(x,y,len1,len2):
+        if (len1 <1):
+            return(y)
+        elif (len2<1):
+            return (x)
+        elif (x[0] <y[0]):
+            return x[0] + foreach(string_get_at(x,1,string_len(x)),y,len1-1,len2)
+        else:
+            return y[0] + foreach(x,string_get_at(y,1,string_len(y)),len1,len2-1)  
+    return (foreach())
+    
     
 
 def string_len(x):
@@ -25,5 +28,3 @@ def string_len(x):
 
 def string_get_at(x, start, finish):
     return x[start:finish]
-
-print(string_merge("135", "2468"))
