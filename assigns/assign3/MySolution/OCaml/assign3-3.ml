b2bfd9593b12a12f77c5e00d98ac515ae527ccfd
PrathmeshS <prs.lowell@gmail.com>
2023-10-04 01:49:27 -0400

    updated 3-3

--- assigns/assign3/MySolution/OCaml/assign3-3.ml
+++ assigns/assign3/MySolution/OCaml/assign3-3.ml
@@ -16,10 +16,14 @@ let list_subsets (xs: 'a list): 'a list list =
      list_append (acc) (list_map(acc)(fun xs -> i::xs)))
 ;;
 
-
+let getTails(xss: 'a list list):  'a list list = 
+  match xss with 
+  | []::a-> a
+  | _ ->[]
+;;
 
 
 let list_nchoose (xs: 'a list)(n0: int): 'a list list =
   let list_of_lists = list_subsets(xs) in 
-    list_foldright (list_of_lists) ([[]]) (fun i acc -> if (list_length(i) <=n0) then i::acc else acc)
+    getTails (list_foldright (list_of_lists) ([[]]) (fun i acc -> if (list_length(i) <=n0) then i::acc else acc))
 ;;
