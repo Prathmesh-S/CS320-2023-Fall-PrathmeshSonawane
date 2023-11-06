69dcadb1c3b96d43df1e7ec0707ac960a8a8fc21
Prathmesh Sonawane <61214417+Prathmesh-S@users.noreply.github.com>
2023-11-06 17:29:29 -0500

    Added OCAML

---
+++ assigns/assign5/MySolution/OCaml/assign5.ml
@@ -0,0 +1,80 @@
+#use "./../../../../classlib/OCaml/MyOCaml.ml";;
+(*
+#use "./../../../../classlib/OCaml/MyOCaml.ml";;
+Grammar (<expr> is the start symbol)
+
+<digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+<num>   ::= <digit> | <digit><num>
+<expr>  ::= <num> 
+          | (add <exprs> )
+          | (mul <exprs> )
+<exprs> ::= <expr> | <expr><exprs>
+
+*)
+
+type expr =
+  | Int of int       (* 1, 2, 3, 4 ...  *)
+  | Add of expr list (* (add e1 e2 ...) *)
+  | Mul of expr list (* (mul e1 e2 ...) *)
+
+type key = 
+  | ( 
+  | )
+  | a 
+  | m
+;;
+(* turn a string into a list of chars *)
+let string_listize (s : string) : char list =
+  list_make_fwork(fun work -> string_foreach s work)
+
+(* remove blank chars at the front of a list *)
+let rec trim cs =
+  match cs with
+  | [] -> cs
+  | '\n' :: cs -> trim cs
+  | '\t' :: cs -> trim cs
+  | '\r' :: cs -> trim cs
+  | ' ' :: cs -> trim cs
+  | _ -> cs
+
+(* Please implement a parse function. When given a valid string according
+   to the grammar, your parse function returns an expr value encoding the
+   expression.
+
+   Example (Accpeted Strings):
+   parse "(add 1 2 3)" = Some (Add [Int 1; Int 2; Int 3])
+   parse "(mul (add 1 2) 3 (mul 1))" = Some (Mul [Add [Int 1; Int 2]; Int 3; Mul [Int 1]])
+
+   Example (Rejected Strings):
+   parse "()" = None
+   parse "(add)" = None
+   parse "(add 1 2))" = None
+   parse "((mul 1 2)" = None
+
+*)
+
+let parse_digit (x:char) : expr option =
+  let t = int_of_char x - 48 in
+  if (0 <= t) && (t <= 9) then Some (Int t) else None 
+;;
+
+let rec parse_num (cs) =
+  match trim cs with
+  | [] -> None
+  | _ -> (
+    match cs with
+    | a::b -> parse_digit(a)::pare
+  )
+  ;;
+
+let parse_term (x:char): expr option = 
+  match x with
+
+
+let parse_expr (x:char list): expr option = 
+ None
+;;
+
+let parse (s : string) : expr option = 
+  parse_expr(string_listize (s))
+;;
